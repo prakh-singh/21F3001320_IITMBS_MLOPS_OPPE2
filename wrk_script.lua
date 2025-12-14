4aa5a81d6432e72f79df09d04dc4060fdf99cf00
prakh-singh <21f3001320@ds.study.iitm.ac.in>
2025-12-14 13:54:46 +0000

    Milestone 7: High concurrency performance test using wrk

---
+++ wrk_script.lua
@@ -0,0 +1,20 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+
+wrk.body = [[
+{
+  "age": 63,
+  "gender": "male",
+  "cp": 3,
+  "trestbps": 145,
+  "chol": 233,
+  "fbs": 1,
+  "restecg": 0,
+  "thalach": 150,
+  "exang": 0,
+  "oldpeak": 2.3,
+  "slope": 0,
+  "ca": 0,
+  "thal": 1
+}
+]]
